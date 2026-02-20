68371d40cbbe1d76cef0f34f031b353c119a3b24
aviaarrr005 <avilashrout2005@gmail.com>
2026-02-20 09:39:00 +0530

    Day 2

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pattern2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
